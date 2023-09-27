1eb1e9cd9ae63eebd9c03c34ed1f8b10af41d63a
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 20:43:29 -0300

    commit add. 2

--- conta-banco/src/ContaBanco.java
+++ conta-banco/src/ContaBanco.java
@@ -1,35 +1,51 @@
 import java.util.Scanner;
-
 public class ContaBanco {
 
-  public static void main(String[] args) {
-    try (// Lendo os dados de Entrada:
-    Scanner scanner = new Scanner(System.in)) {
-      int numeroConta = scanner.nextInt();
-      scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
-      String nomeTitular = scanner.nextLine();
-      double saldo = scanner.nextDouble();
+    public static void main(String[] args) {
+        try (// Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in)) {
+          
+          // Solicitação ao usuário 
+          System.out.print("Por favor, digite o número da sua conta: ");
+          int numeroConta = scanner.nextInt();
+          scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
 
-      // Criar uma instância de "ContaBancaria" com os valores de Entrada.
-      ContaBancaria conta = new ContaBancaria(numeroConta, nomeTitular, saldo);
+          System.out.println();
 
-      System.out.println("Informacoes:");
-      // Imprimir as informações da conta usando o objeto criado acima.
-      System.out.println("Conta: " + conta.numero);
-      System.out.println("Titular: " + conta.titular);
-      System.out.println("Saldo: R$ " + conta.saldo);
+          // Solicitação ao usuário 
+          System.out.print("Por favor, digite o seu nome: ");
+          String nomeTitular = scanner.nextLine();
+
+          System.out.println();
+
+          // Solicitação ao usuário 
+          System.out.print("Por favor, digite o seu saldo: ");
+          double saldo = scanner.nextDouble();
+
+          System.out.println();
+
+          ContaBancaria conta = new ContaBancaria(numeroConta, nomeTitular, saldo);
+
+          System.out.println("Informacoes:");
+
+          System.out.println();
+
+          // Imprimindo as informações da conta usando o objeto criado acima.
+          System.out.println("Conta: " + conta.numero);
+          System.out.println("Titular: " + conta.titular);
+          System.out.println("Saldo: R$ " + conta.saldo);
+        }
     }
-  }
 }
 
 class ContaBancaria {
-  int numero;
-  String titular;
-  double saldo;
+    int numero;
+    String titular;
+    double saldo;
 
-  public ContaBancaria(int numero, String titular, double saldo) {
-    this.numero = numero;
-    this.titular = titular;
-    this.saldo = saldo;
-  }
+    public ContaBancaria(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
 }
